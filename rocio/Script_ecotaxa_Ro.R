c65912c2d8f5b9e32b5b71e088f12909d508aa62
Stevan Earl <stevan.earl@asu.edu>
2025-02-03 12:21:37 -0700

    docs: sprinkling questions for rocio

--- rocio/Script_ecotaxa_Ro.R
+++ rocio/Script_ecotaxa_Ro.R
@@ -102,6 +102,8 @@ data_p[, c("Temp", "Sal", "Fluor", "O2", "Depth_max", "Depth_min")] <- vegan::de
 spe <- data_p[, 23:89] # Specify the columns of the morphological variables 
 YJ_trans <- cellWise::transfo(spe, type = "YJ")$Y # Perform a Yeo-Johnson transformation on the morphological variables 
 
+# SRE: cellWise produces a list of which the Y object is the data matrix
+
 data_t1 <- data_p |>
   # Remove the original morphological variables from the data
   dplyr::select(-one_of(colnames(spe))) |>
@@ -110,6 +112,11 @@ data_t1 <- data_p |>
 
 # Store the transformed data in a separate object
 YJ_data <- YJ_trans
+
+# SRE: at this point we have (1) environmental variables that have been
+# standardized with deostand, (2) morphological variables that have been
+# transormed per Yeo-Johnson both as (a) part of data_t1 and (b) as a
+# standalone object (YJ_data), and (3) associated tow details.
 
 ## Morphological Variable Selection ####
 
@@ -215,6 +222,10 @@ data_for_pca <- as.data.frame(
 data_for_pca <- data_for_pca |>
   dplyr::mutate(across(everything(), as.numeric))
 
+# SRE: PCA on environmental and morphological variables only on samples
+# identified as Calanoida. Curious mixing of dependent and independent
+# variables in a single analysis. Maybe a question for Rocio.
+
 # Perform PCA
 res.pca <- FactoMineR::PCA(
   X          = data_for_pca,
@@ -227,6 +238,9 @@ res.pca <- FactoMineR::PCA(
 # Get PCA variables and create PCA plot with images
 pca.vars <- rbind(res.pca$var$coord, res.pca$quanti.sup$coord) |>
   as.data.frame()
+
+# SRE: I can run the PCA but am unable to plot using the supplied code without
+# the image data.
 
 images <- morphr::ggmorph_tile(
   space       = res.pca,
